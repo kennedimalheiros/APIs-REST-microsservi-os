3ee724a2d9d48309491d23f37f3421d3ff5affde
kennedimalheiros <kennedimalheiros@gmail.com>
2020-10-31 19:19:31 -0300

    Implementando coveragerc no test

--- server/product/models.py
+++ server/product/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    barcode = models.PositiveIntegerField('Código de Barras', primary_key=True)
+    name = models.CharField('Produto', max_length=100)
+
+    def __str__(self):
+        return f'({self.barcode}) - {self.name}'
--- server/product/tests.py
+++ server/product/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+
+class ProductAPITest(APITestCase):
+
+    def test_url_api_exist(self):
+        response = self.client.get('/product/')
+        self.assertEqual(response.status_code, 200)
